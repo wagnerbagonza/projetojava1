0864a91e1e0d2d46df45e32b6e9a28372672a6dd
wagnerbagonza <37303948+wagnerbagonza@users.noreply.github.com>
2022-04-24 17:10:49 -0300

    Add atributo idade na Classe Pessoa

--- projetojava1/src/curso/java/wagner/Pessoa.java
+++ projetojava1/src/curso/java/wagner/Pessoa.java
@@ -4,6 +4,15 @@ public class Pessoa {
 
 	private String nome;
 	private String sobrenome;
+	private int idade;
+
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public String getNome() {
 		return nome;
 	}
